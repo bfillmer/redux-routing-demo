61a68179ead3da5d167b027524d190c1b40216b1
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-06-13 14:12:31 -0600

    Leverage Link component.

--- src/view/Editor/View.js
+++ src/view/Editor/View.js
@@ -1,9 +1,10 @@
 
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export const View = () => (
   <div>
     <h1>EDITOR VIEW</h1>
-    <a href='/login'>Login</a>
+    <Link to='/login'>Login</Link>
   </div>
 )
--- src/view/Login/View.js
+++ src/view/Login/View.js
@@ -1,9 +1,10 @@
 
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export const View = () => (
   <div>
     <h1>LOGIN VIEW</h1>
-    <a href='/editor'>Editor</a>
+    <Link to='/editor'>Editor</Link>
   </div>
 )
