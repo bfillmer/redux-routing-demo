7a5cd35de7faac80422ce7c62b38e52d56d3fe38
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-06-13 09:28:48 -0600

    Delays on state updates.

--- src/state/sagas.js
+++ src/state/sagas.js
@@ -1,5 +1,6 @@
 
 import {fork, put} from 'redux-saga/effects'
+import {delay} from 'redux-saga'
 import {router, createBrowserHistory} from 'redux-saga-router'
 
 import {routes, sagaMap} from 'routes'
@@ -13,16 +14,25 @@ export function* rootSaga () {
 // @NOTE Would move these to their own files, import actions from the central
 // actions and obviously have types setup so the reducer, sagas, and actions all
 // import them for consistency.
+
+// @NOTE Would handle loading component display here at the saga level.
+
 export function* loginSaga () {
+  // Display loading component.
+  yield delay(10000)
   yield put({
     type: 'NAVIGATE',
     payload: routes.login
   })
+  // Hide loading component.
 }
 
 export function* editorSaga () {
+  // Display loading component.
+  yield delay(10000)
   yield put({
     type: 'NAVIGATE',
     payload: routes.editor
   })
+  // Hide loading component.
 }
